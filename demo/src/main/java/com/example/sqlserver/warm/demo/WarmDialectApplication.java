715cc102730104841e9587cb3faf46638d187dd2
Rodrigo Darti da Costa <darckyn@users.noreply.github.com>
2023-12-26 09:39:10 -0300

    Add project

---
+++ demo/src/main/java/com/example/sqlserver/warm/demo/WarmDialectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlserver.warm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmDialectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmDialectApplication.class, args);
+	}
+
+}
